d6395ac72e928b0cab5408a4baeb67b5a25cef28
Kristian Alonso <117874594+Kris1980k@users.noreply.github.com>
2026-06-27 14:46:52 -0600

    feat: implement getProducts, work on getView
    Signed-off-by: Kristian Alonso <117874594+Kris1980k@users.noreply.github.com>

--- client.js
+++ client.js
@@ -1,17 +1,33 @@
-
-import { Client } from 'pg'
-
-export default async function myPostgresClient(){
-    console.log('holaaaa' , process.env.DB_USER);
-
+async function getProducts(){
+    
+    const Client = require('pg').Client
+    
     const CLIENT = await new Client({
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
     }).connect()
 
-    const res = await CLIENT.query('select * from rivals')
-    console.log(res.rows);
+    const res = await CLIENT.query('select * from products')
+    
     await CLIENT.end()
+    return res.rows;
 }
 
+async function getView(){
+    
+    const Client = require('pg').Client
+    
+    const CLIENT = await new Client({
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    }).connect()
+
+    const res = await CLIENT.query('select * from view_registers')
+    
+    await CLIENT.end()
+
+    return res.rows;
+}
+module.exports = { getProducts, getView }
--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-import 'dotenv/config'
-import  myPostgresClient  from './client.js'
+require('dotenv').config()
 
-myPostgresClient()
+const {getView, getProducts} = require('./client.js');
+const express = require('express');
+const path = require('node:path')
+const app = express();
+const port = 3000;;
+
+
+app.engine('.html', require('ejs').__express);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'html');
+
+app.get('/home', (req,res) => {
+    const getProductList = getProducts().then((productList) => {
+        console.log(productList);
+        
+        res.render('index', {productList:productList});
+    });    
+})
+
+app.get('/view', (req,res) => {
+    const getViewList = getView();
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
